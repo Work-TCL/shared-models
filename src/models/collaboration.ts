db8a0baffa7811811126f5a5385bf4bb56251454
Akshay <akshay10sangani@gmail.com>
2025-03-31 23:12:57 +0530

    collaboration modal added

---
+++ src/models/collaboration.ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const CollaborationSchema = new Schema(
+  {
+    creatorId: {
+      type: Schema.Types.ObjectId,
+      ref: "Creator",
+      required: true,
+    },
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    vendorId: {
+      type: Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
+    },
+    collaborationStatus: {
+      type: String,
+      enum: ["PENDING", "ACTIVE", "REJECTED"],
+      default: "PENDING",
+    },
+    utmLink: {
+      type: String,
+      default: null, // Will be set after UTM generation
+    },
+    discountType: {
+      type: String,
+      enum: ["PERCENTAGE", "FIXED_AMOUNT"],
+      required: true,
+    },
+    discountValue: {
+      type: Number,
+      required: true,
+    },
+    couponCode: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    commissionPercentage: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+    // shop: {
+    //   type: String,
+    //   required: true,
+    // }
+  },
+  { versionKey: false, timestamps: true }
+);
+
+
+// export const CollaborationModel = mongoose.model("Collaboration", CollaborationSchema);
+export default CollaborationSchema;
