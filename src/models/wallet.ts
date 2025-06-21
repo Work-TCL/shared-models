9c621a5c5ad203ca9fae051b22737d8019aaeb27
Akshay <akshay10sangani@gmail.com>
2025-06-21 12:27:57 +0530

    blocked amount key added

--- src/models/wallet.ts
+++ src/models/wallet.ts
@@ -5,8 +5,8 @@ const { Schema } = Mongoose;
 const WalletSchema = new Schema(
     {
         accountId: { type: Schema.Types.ObjectId, ref: "Account", required: true },
-        balance: { type: Number, default: 0 },
-        blockedBalance: { type: Number, default: 0 }
+        blockedBalance: { type: Number, default: 0 },
+        balance: { type: Number, default: 0 }
     },
     { timestamps: true, versionKey: false } // Automatically adds createdAt & updatedAt
 );
