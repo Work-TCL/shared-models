8a452eb024ebc898129736bc20c0474ed6447017
Akshay <akshay10sangani@gmail.com>
2025-07-21 23:14:18 +0530

    mapping table introduced

--- src/models/category.ts
+++ src/models/category.ts
@@ -5,15 +5,19 @@ const { Schema } = Mongoose;
 // Category Schema
 const CategorySchema = new Schema(
   {
-   name:{
-    type: String,
-    required: true,
-    unique: true,
-   },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     parentId: {
       type: Schema.Types.ObjectId,
       ref: "Category",
       default: null,
+    },
+    type: {
+      type: String,
+      enum: ["creator", "vendor"],
     },
   },
   { versionKey: false, timestamps: true }
