f8b2cc6e9b0f6bda9383801a333cd6662ab3e780
Akshay <akshay10sangani@gmail.com>
2026-03-07 10:45:07 +0530

    Token schema added

---
+++ src/models/tokens.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const TokenSchema = new Schema(
+  {
+    accountId: {
+      type: Schema.Types.ObjectId,
+      ref: "Account",
+      required: true,
+    },
+    platform: {
+      type: String,
+      enum: ["web", "ios", "android"],
+    },
+    token: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: "30d",
+    },
+  },
+  { versionKey: false },
+);
+
+TokenSchema.index({ accountId: 1 });
+export default TokenSchema;
